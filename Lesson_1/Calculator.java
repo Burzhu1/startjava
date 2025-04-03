80d0c61e15d5c8892172c6e0665ab502e92e1ef8
Burzhu1 <pzerg72@gmail.com>
2025-04-03 18:13:33 +0300

    Исправил

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -4,45 +4,36 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
-        double num1 = scanner.nextInt();
+        int num1 = scanner.nextInt();
         System.out.print("Введите оператор +, -, *, /, %, ^ : ");
         char operator = scanner.next().charAt(0);
         System.out.print("Введите число: ");
-        double num2 = scanner.nextInt();
-        double result;
-        if (operator == '+') {
-            result = num1 + num2;
-        } else if (operator == '-') {
-            result = num1 - num2;
-        } else if (operator == '*') {
-            result = num1 * num2;
-        } else if (operator == '/') {
-            if (num1 == 0 || num2 == 0) {
-                System.out.println("Ошибка, вводимое число не может быть нулём");
-                return;
-            }
-            result = num1 / num2;
-        } else if (operator == '%') {
-            if (num1 == 0 || num2 == 0) {
-                System.out.println("Ошибка, вводимое число не может быть нулём");
-                return;
-            }
-            result = num1 % num2;
-        } else if (operator == '^') {
-            if (num1 == 0) {
-                System.out.println("Ошибка, вводимое число не может быть нулём");
-                return;
-            }
-            result = 1;
-            for (int i = 0; i < num2; i++) {
-                result *= num1;
-            }
-        } else {
-            System.out.println("Ошибка, неизвестный оператор");
+        int num2 = scanner.nextInt();
+        int result;
+        
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("Ошибка, деление на ноль запрещено");
             return;
+            } else if (operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                result = num1 / num2;
+            } else if (operator == '%') {
+                result = num1 % num2;
+            } else if (operator == '^') {
+                result = 1;
+                for (int i = 0; i < num2; i++) {
+                    result *= num1;
+                }
+            } else {
+        System.out.println("Ошибка, неизвестный оператор");
+        return;
         }
-        int convert = (int) result;
-        System.out.println(num1 + " " + operator + " " + num2 + " = " + convert);
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
         scanner.close();
     }
 }
