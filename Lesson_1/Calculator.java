a556fee69f8cb8fc0ac24420c09968a86c5104c0
Burzhu1 <pzerg72@gmail.com>
2025-04-03 18:26:03 +0300

    Исправил

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -14,24 +14,24 @@ public class Calculator {
         if (num1 == 0 || num2 == 0) {
             System.out.println("Ошибка, деление на ноль запрещено");
             return;
-            } else if (operator == '+') {
-                result = num1 + num2;
-            } else if (operator == '-') {
-                result = num1 - num2;
-            } else if (operator == '*') {
-                result = num1 * num2;
-            } else if (operator == '/') {
-                result = num1 / num2;
-            } else if (operator == '%') {
-                result = num1 % num2;
-            } else if (operator == '^') {
-                result = 1;
-                for (int i = 0; i < num2; i++) {
-                    result *= num1;
-                }
-            } else {
-        System.out.println("Ошибка, неизвестный оператор");
-        return;
+        } else if (operator == '+') {
+            result = num1 + num2;
+        } else if (operator == '-') {
+            result = num1 - num2;
+        } else if (operator == '*') {
+            result = num1 * num2;
+        } else if (operator == '/') {
+            result = num1 / num2;
+        } else if (operator == '%') {
+            result = num1 % num2;
+        } else if (operator == '^') {
+            result = 1;
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
+        } else {
+            System.out.println("Ошибка, неизвестный оператор");
+            return;
         }
         System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
         scanner.close();
