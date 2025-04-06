abc9db9927af1e42f28a1187a77b5a0d5ae4e79a
Burzhu1 <pzerg72@gmail.com>
2025-04-06 23:11:12 +0300

    Улучшил согласно 5 теме

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,16 +1,18 @@
 public class WolfTest {
     public static void main(String[] args) {
-        Wolf wolfOne = new Wolf();
-        wolfOne.sex = "самец";
-        wolfOne.nickname = "Фенрир";
-        wolfOne.weight = 60;
-        wolfOne.age = 4;
-        wolfOne.color = "Чёрный";
-        System.out.println("Пол волка " + wolfOne.sex);
-        System.out.println("Кличка " + wolfOne.nickname);
-        System.out.println("Вес " + wolfOne.weight);
-        System.out.println("Возраст " + wolfOne.age);
-        System.out.println("Окрас " + wolfOne.color);
+        Wolf wolfOne = new Wolf("самец", "Фенрир", 60, 4, "Чёрный");
+
+        String wolfOneSex = wolfOne.getSex();
+        String wolfOneNickname = wolfOne.getNickname();
+        int wolfOneWeight = wolfOne.getWeight();
+        int wolfOneAge = wolfOne.getAge();
+        String wolfOneColor = wolfOne.getColor();
+
+        System.out.println("Пол волка: " + wolfOneSex);
+        System.out.println("Кличка: " + wolfOneNickname);
+        System.out.println("Вес: " + wolfOneWeight);
+        System.out.println("Возраст: " + wolfOneAge);
+        System.out.println("Окрас: " + wolfOneColor);
         wolfOne.walk();
         wolfOne.sit();
         wolfOne.run();
