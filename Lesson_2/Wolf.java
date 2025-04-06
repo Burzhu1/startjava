607d70878355c9b25bc3821ee0134c54f6aa2fc8
Burzhu1 <pzerg72@gmail.com>
2025-04-06 23:10:36 +0300

    Улучшил согласно 5 теме

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,9 +1,61 @@
 public class Wolf {
-    String sex;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private String sex;
+    private String nickname;
+    private int weight;
+    private int age;
+    private String color;
+
+    public Wolf(String sex, String nickname, int weight, int age, String color) {
+        this.sex = sex;
+        this.nickname = nickname;
+        this.weight = weight;
+        setAge(age);
+        this.color = color;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex() {
+        this.sex = sex;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname() {
+        this.nickname = nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight() {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+            return;
+        }
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor() {
+        this.color = color;
+    }
 
     public void walk() {
         System.out.println("Идёт");
