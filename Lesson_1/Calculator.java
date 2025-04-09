91399d49a16a1ec1d2ece8855fb0943b9e53e2be
Burzhu1 <pzerg72@gmail.com>
2025-04-09 18:55:10 +0300

    Исправил

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -3,38 +3,44 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Введите число: ");
         int num1 = scanner.nextInt();
-
         System.out.print("Введите оператор +, -, *, /, %, ^ : ");
         char operator = scanner.next().charAt(0);
-
         System.out.print("Введите число: ");
         int num2 = scanner.nextInt();
-
         if ((operator == '/' || operator == '%') && num2 == 0) {
             throw new IllegalArgumentException("Деление на ноль запрещено");
         }
-
-        if ("+-*/%^".indexOf(operator) == -1) {
-            throw new IllegalArgumentException("Неизвестный оператор: " + operator);
-        }
-
         int result = 0;
-
-        if (operator == '+') result = num1 + num2;
-        if (operator == '-') result = num1 - num2;
-        if (operator == '*') result = num1 * num2;
-        if (operator == '/') result = num1 / num2;
-        if (operator == '%') result = num1 % num2;
-        if (operator == '^') {
-            result = 1;
-            for (int i = 0; i < num2; i++) {
-                result *= num1;
-            }
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '%':
+                result = num1 % num2;
+                break;
+            case '^':
+                result = 1;
+                if (num2 >= 0) {
+                    for (int i = 0; i < num2; i++) {
+                        result *= num1;
+                    }
+                } 
+                break;
+            default:
+                System.out.println("Неизвестный оператор: " + operator);
+                return;
         }
-
         System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
         scanner.close();
     }
