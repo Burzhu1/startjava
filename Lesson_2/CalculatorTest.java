7516cc61ea2c3522b3fe17d0355441de1b389c3d
Burzhu1 <pzerg72@gmail.com>
2025-04-09 17:58:39 +0300

    Внёс изменения

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -8,48 +8,47 @@ public class CalculatorTest {
         do {
             System.out.print("Введите первое число: ");
             int num1 = scanner.nextInt();
-
             System.out.print("Введите оператор +, -, *, /, %, ^ : ");
             char operator = scanner.next().charAt(0);
-
             System.out.print("Введите второе число: ");
             int num2 = scanner.nextInt();
-
-            // Проверка на деление на 0
             if (operator == '/' && num2 == 0) {
                 System.out.println("Ошибка: деление на ноль запрещено");
-            } else if ("+-*/%^".indexOf(operator) == -1) {
-                System.out.println("Ошибка: операция " + operator + " не поддерживается");
-            } else {
-                int result = 0;
-
-                switch (operator) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                    case '%':
-                        result = num1 % num2;
-                        break;
-                    case '^':
-                        result = 1;
+            }
+            double result = 0;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                case '%':
+                    result = num1 % num2;
+                    break;
+                case '^':
+                    result = 1;
+                    if (num2 >= 0) {
                         for (int i = 0; i < num2; i++) {
                             result *= num1;
                         }
-                        break;
-                }
-
-                System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+                    } else {
+                        for (int i = 0; i < -num2; i++) {
+                            result *= num1;
+                        }
+                        result = 1 / result;
+                    }
+                    break;
+                default:
+                    System.out.println("Ошибка: операция " + operator + " не поддерживается");
             }
-
+            System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
             while (true) {
                 System.out.print("Хотите продолжить? [yes/no]: ");
                 repeat = scanner.next().toLowerCase();
@@ -59,7 +58,6 @@ public class CalculatorTest {
                 }
                 System.out.println("Пожалуйста, введите только 'yes' или 'no'.");
             }
-
         } while (repeat.equals("yes"));
 
         System.out.println("Программа завершена.");
